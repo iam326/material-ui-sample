3b42417d438647203c78c9d0cbbf27519eab70f0
t.takahashi <takahashi.developer@gmail.com>
2020-01-15 01:06:40 +0900

    modify card layout

--- frontend/src/Item.tsx
+++ frontend/src/Item.tsx
@@ -1,10 +1,9 @@
 import React from 'react';
 import 'typeface-roboto';
 import { makeStyles } from '@material-ui/core/styles';
-import Typography from '@material-ui/core/Typography';
 import Card from '@material-ui/core/Card';
 import CardMedia from '@material-ui/core/CardMedia';
-import CardContent from '@material-ui/core/CardContent';
+import CardHeader from '@material-ui/core/CardHeader';
 import CardActions from '@material-ui/core/CardActions';
 import IconButton from '@material-ui/core/IconButton';
 import ShareIcon from '@material-ui/icons/Share';
@@ -26,7 +25,6 @@ interface Props {
 
 const useStyles = makeStyles({
   card: {
-    paddingTop: '10px',
     marginBottom: '10px',
     minWidth: 320,
     maxWidth: 640
@@ -41,19 +39,17 @@ function Item(props: Props) {
   const classes = useStyles();
   return (
     <Card className={classes.card}>
+      <CardHeader
+        titleTypographyProps={{variant:'body1'}}
+        subheaderTypographyProps={{variant:'body2'}}
+        title={props.title}
+        subheader={props.body}
+      />
       <CardMedia
         className={classes.media}
         image={props.image}
         title="logo"
       />
-      <CardContent>
-        <Typography variant="h5" component="h2">
-          {props.title}
-        </Typography>
-        <Typography variant="body2" color="textSecondary" component="p">
-          {props.body}
-        </Typography>
-      </CardContent>
       <CardActions disableSpacing>
         <IconButton aria-label="thumb-up">
           <ThumbUpIcon
